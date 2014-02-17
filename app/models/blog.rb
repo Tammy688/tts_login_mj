4ecfac1894552a541957f406156ccb748bb1200f
TammyLLC <me@TammyLLC.com>
2014-02-17 15:49:48 -0500

    first commit

---
+++ app/models/blog.rb
@@ -0,0 +1,5 @@
+class Blog < ActiveRecord::Base
+	belongs_to :user
+	has_many :blog_entries
+	
+end
---
+++ app/models/blog_comment.rb
@@ -0,0 +1,5 @@
+class BlogComment < ActiveRecord::Base
+	belongs_to :blog_entry
+	belongs_to :user
+
+end
